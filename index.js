a3ffdd714be980b86209e107b1004de1f4538a2a
Kristian Lachev <i435061@fhict.nl>
2024-04-25 22:13:53 +0300

    add Total amount drank and design fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 let countEl = document.getElementById("count-el") 
 let count = 0 
 let saveEl = document.getElementById("save-el") 
+let totalEl = document.getElementById("total-el")
+let totalNumber = 0
 
  
 
@@ -13,5 +15,12 @@ function increment() {
 function save() {
     let  saveCount = count * 50 + " ml - " 
     saveEl.textContent += saveCount 
+    totalNumber += count * 50
     countEl.textContent = count = 0
+    if (totalNumber >= 1000){
+        totalEl.textContent = totalNumber / 1000 + " L"
+    }
+    else{
+        totalEl.textContent = totalNumber + " ml"
+    }
 }
